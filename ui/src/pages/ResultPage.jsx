c0e3004a7aeea5366735d1224b6d2de1b1ff213a
Andrew Chen <che0030main@gmail.com>
2024-03-19 20:59:20 +1100

    uploading again commit 3

--- ui/src/pages/ResultPage.jsx
+++ ui/src/pages/ResultPage.jsx
@@ -1,5 +1,9 @@
+
 function ResultPage() {
-    return <div>Result Page</div>;    //renders result page with placeholder text "results page" 
-  }
-  
+  return <div>Result Page</div>;    //renders result page with placeholder text "results page" 
+}
+
 export default ResultPage;
+
+
+app.js
