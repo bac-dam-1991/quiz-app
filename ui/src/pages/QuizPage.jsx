4026fb38c685255a4144ba443a5ba16b26380cd9
bactrachdam <bac.dam.1991@gmail.com>
2024-03-26 21:09:19 +1100

    Minor tweaks.

--- ui/src/pages/QuizPage.jsx
+++ ui/src/pages/QuizPage.jsx
@@ -1,14 +1,5 @@
-// Ticket: Create a placeholder component for the quiz page. When the state has the value of "started", the component would be rendered.
-
 import { Typography } from "@mui/material";
-import { useState } from "react";
 
-function QuizPage() {
-  // appState can be "started" | "finished"
-  const [appState, setAppState] = useState();
-
-  if (appState === "started") {
-    return <Typography variant="h1">Placeholder Component</Typography>;
-  }
-}
-export default QuizPage;
+export const QuizPage = () => {
+  return <Typography variant="h1">Placeholder Component</Typography>;
+};
