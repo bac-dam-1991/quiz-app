2f6e23dc9bd6e0e8e2501ac1c0a57b993da8631b
bac-dam-1991 <66862347+bac-dam-1991@users.noreply.github.com>
2024-03-26 21:20:51 +1100

    Create results page (#3)
    * test change 1
    
    * no changes again
    
    * add dummy code
    
    * remove all text again
    
    * upload answer for when state value started, render result page with placeholder
    
    * uploading again
    commit 3
    
    * upload changes 1
    
    * Resolved conflict.
    
    ---------
    
    Co-authored-by: Andrew Chen <che0030main@gmail.com>

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,9 @@
 import { Button, Typography, Container, Stack } from "@mui/material";
 import { useState } from "react";
 import { QuizPage } from "./pages/QuizPage";
+import { ResultPage } from "./pages/ResultPage";
 
 function App() {
-  // appState can be "started" | "finished"
   const [appState, setAppState] = useState();
 
   return (
@@ -12,12 +12,13 @@ function App() {
         <Typography variant="h3">An awesome quiz app</Typography>
         {appState === "started" ? (
           <QuizPage />
+        ) : appState === "finished" ? (
+          <ResultPage />
         ) : (
           <Button variant="contained" onClick={() => setAppState("started")}>
             Start quiz
           </Button>
         )}
-
         <Typography variant="caption">
           Built by Bac, Andrew, Mei and Tin
         </Typography>
