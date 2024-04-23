824a6945c7fb5991d9d4331d8142b8956d995168
Bac Trach Dam <bac.dam.1991@gmail.com>
2024-04-23 20:41:08 +1000

    Added design to code base.

--- test.js
+++ test.js
@@ -2,27 +2,29 @@
 
 let _firstName = "John";
 
-const person = {
+const person01 = {
   firstName: _firstName,
 };
 
-console.log("_firstName", _firstName);
-console.log("person.firstName", person.firstName);
+// console.log("_firstName", _firstName);
+// console.log("person.firstName", person.firstName);
 
-_firstName = "Jane";
+// _firstName = "Jane";
 
-console.log("_firstName", _firstName);
-console.log("person.firstName", person.firstName); // 1 x John, 2 x Jane
+// console.log("_firstName", _firstName);
+// console.log("person.firstName", person.firstName); // 1 x John, 2 x Jane
 
 // 1. Answer what will be printed to console
 // 2. Visually draw what happens
 const person02 = person01;
 
-person.name = "Jill";
+person01.name = "Jill";
 person02.name = "Jack";
-console.log("person.name", person.name);
+console.log("person01.name", person02.name);
 console.log("person02.name", person02.name);
 
 // To run this code: node test
 // To view the diagram, make sure the Draw.io Integration extension by Henning Dieterichs is installed
 // Click on the file, it should open the diagram in draw.io
+
+state management
