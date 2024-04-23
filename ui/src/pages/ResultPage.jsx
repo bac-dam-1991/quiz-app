82b1428dff492fa20f67713fe5f2a805fcc3da81
Andrew Chen <che0030main@gmail.com>
2024-04-23 21:27:32 +1000

    skeleton framework html for results page

--- ui/src/pages/ResultPage.jsx
+++ ui/src/pages/ResultPage.jsx
@@ -1,3 +1,28 @@
-export const ResultPage = () => {
-  return <div>Result Page</div>; //renders result page with placeholder text "results page"
-};
+function ResultPage() {
+  return (
+    <>
+      <h1>Good Job!</h1>
+      <header>
+        <img src="" alt="" />
+      </header>
+      <main>
+        <div className="correct"></div>
+        <h3>correct</h3>
+        <div className="correct-box"></div>
+        <div className="incorrect">
+          <h3>incorrect</h3>
+          <div className="incorrect-box"></div>
+        </div>
+        <div className="score">
+          <h3>score</h3>
+          <h1>80%</h1>
+          {/* this would return 80%? use static placeholder for now? */}
+        </div>
+
+        <button id="done-button">Done</button>
+      </main>
+    </>
+  );
+}
+
+export default ResultPage;
