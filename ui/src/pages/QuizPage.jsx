415cfe8e0e0595f4a67e915062d3674506622b9c
Bac Trach Dam <btdtech2021@gmail.com>
2024-06-11 20:36:03 +1000

    Refactored code.

--- ui/src/pages/QuizPage.jsx
+++ ui/src/pages/QuizPage.jsx
@@ -5,27 +5,40 @@ import "./QuizPage.css";
 export const QuizPage = () => {
   const [selected, setSelected] = useState(0);
   const [questionNumber, setQuestionNumber] = useState(0);
+  const { question, answer, options } = Questions[questionNumber];
 
   return (
     <Fragment>
       <img alt=""></img>
 
       <main>
-        <h1>Question {questionNumber +1} of {Questions.length}</h1>
-        <p className="question">
-          {Questions[questionNumber].question}
-        </p>
+        <h1>
+          Question {questionNumber + 1} of {Questions.length}
+        </h1>
+        <p className="question">{question}</p>
 
         <div className="answers">
-          {Questions[questionNumber].options.map((answer) => {
+          {options.map(({ value, label }) => {
+            let className = "";
+            const isMatching = selected === value;
+            const isCorrect = selected === answer;
+
+            if (isMatching) {
+              if (isCorrect) {
+                className = "correct-answer";
+              } else {
+                className = "wrong-answer";
+              }
+            }
+
             return (
               <button
-                key={answer.value}
-                name={answer.value}
-                className={selected === answer.value && selected === Questions[questionNumber].answer ? "correct-answer" : selected === answer.value && selected !== Questions[questionNumber].answer ? "wrong-answer" : ""}
-                onClick={() => setSelected(answer.value)}
+                key={value}
+                name={value}
+                className={className}
+                onClick={() => setSelected(value)}
               >
-                {answer.label}
+                {label}
               </button>
             );
           })}
